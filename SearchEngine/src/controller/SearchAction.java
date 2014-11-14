0a71d2255568b9700b7b67565597165d3d3e685a
Jaf270 <thomas.losbar@gmail.com>
2014-11-14 01:43:50 +0100

    drunk in love

---
+++ SearchEngine/src/controller/SearchAction.java
@@ -0,0 +1,42 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import analysis.AnalyseResults;
+import annotation.SearchData;
+import service.Service;
+
+class SearchAction extends Action {
+
+	public SearchAction() {
+	}
+	@Override
+	public void execute(HttpServletRequest request) {
+		Service s = new Service();
+		System.out.println("booom execute");
+		
+		String requete = request.getParameter("q");
+		String label = request.getParameter("label");
+		
+		//ArrayList<AnalyseResults> results =new ArrayList<AnalyseResults>();
+		
+		AnalyseResults analyseResults =null;
+		
+		try {
+			analyseResults = s.launchSearch(requete,label);
+		} catch (IOException e) {
+			System.out.println("Erreur au lancement de la m�thode launchSearch");
+		}
+		
+		Map<SearchData, List<SearchData>> results = analyseResults.getPagesResults();
+		//exemple
+		//AnalyseResults exmple = results.get(0);
+		request.setAttribute("pages", results);
+	}
+
+}
--- SearchEngineBackend/src/analysis/AnalyseResults.java
+++ SearchEngineBackend/src/analysis/AnalyseResults.java
@@ -8,9 +8,17 @@ import org.json.simple.JSONObject;
 import annotation.SearchData;
 
 public class AnalyseResults {
-	public Map<SearchData, List<SearchData>> pagesResults;
-	public JSONObject objJSON;
+	private Map<SearchData, List<SearchData>> pagesResults;
+	private JSONObject objJSON;
 	
+	public Map<SearchData, List<SearchData>> getPagesResults() {
+		return pagesResults;
+	}
+
+	public JSONObject getObjJSON() {
+		return objJSON;
+	}
+
 	public AnalyseResults(Map<SearchData, List<SearchData>> pagesResults,
 			JSONObject objJSON) {
 		this.pagesResults = pagesResults;
